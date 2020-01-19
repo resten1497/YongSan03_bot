10934a33b197cca26d5856c1283fb8fcb7f717f6
Resten <saup19@naver.com>
2020-01-19 20:05:21 +0900

    deploy_server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import xml2js from "xml2js";
 import express from 'express';
 import bodyParser from 'body-parser';
 import {getBusImage, getBusSize} from "./src/util";
+import {busResultMessage} from "./src/message";
 
 var fs = require('fs');
 const app = express();
@@ -36,7 +37,7 @@ app.post('/', function (req, res) {
 
 });
 
-const parser2 = (data) => {
+const PromiseParser = (data) => {
     return new Promise((resolve, reject) => {
         parser.parseString(data, (err, result) => {
             if (err) {
@@ -48,92 +49,64 @@ const parser2 = (data) => {
 };
 
 app.get('/', asyncHandler(async (req,res)=>{
-    let arsId = '03567';
-    let data = await getBusTime(arsId);
-    const result = await parser2(data);
-    let Object = []
-    let itemList = result.ServiceResult.msgBody.itemList;
-    let arrmsg1 = itemList.arrmsg1;
-    let arrmsg2 =itemList.arrmsg2;
-    let veh1 = itemList.vehId1;
-    let veh2 = itemList.vehId2;
-    Object.stNm = itemList.stNm;
 
-
-    if(arrmsg1 !== "운행종료") {
-        if (arrmsg1 !== "곧 도착" ) {
-            let split_Minute1 = arrmsg1.split('분')
-
-            let Station_result1 = split_Minute1[1].split('[')[1]
-                .slice(0, -1)
-                .replace("번째", "정류장")
-
-            Object.description = "이번 버스 : " + getBusSize(veh1).label + " \n " + Station_result1 + ", " + split_Minute1[0] + "분 후 도착\n";
-
-        } else {
-            Object.description =  getBusSize(veh1).label + " \n " + arrmsg1 ;
-        }
-
-            let split_Minute2 = arrmsg2.split('분')
-            let Station_result2 = split_Minute2[1].split('[')[1]
-                .slice(0, -1)
-                .replace("번째", "정류장")
-
-            console.log(Station_result2 + ", " + split_Minute2[0] + "분 후 도착")
-            Object.description += "다음 버스 : " + getBusSize(veh2).label + " \n " + Station_result2 + ", " + split_Minute2[0] + "분 후 도착\n";
-
-
-            Object.Image = "http://13.125.72.93/img/"+getBusImage(getBusSize(veh1).code, getBusSize(veh2).code);
-            console.log(Object)
-            // console.log(Object);
-
-    }
-    res.send("123")
 }))
 
 app.post('/getBusArriveInformation', asyncHandler( async (req, res) => {
 
     let arsId = req.body.action.params.arsid;
     let data = await getBusTime(arsId);
-    let StNm = "";
-    const result = await parser2(data);
-    let Object = []
-    Object.stNm = result.ServiceResult.msgBody.itemList.stNm;
-    console.log(result.ServiceResult.msgBody.itemList.arrmsg1)
-    console.log(result.ServiceResult.msgBody.itemList.arrmsg2)
-    console.log(Object);
-
-    const responseTeplate = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "basicCard": {
-                        "title": Object.stNm,
-                        "thumbnail": {
-                            "fixedRatio":true,
-                            "height":"720",
-                            "width":"630",
-                            "imageUrl": "http://13.125.72.93/img/yongsan03_big_big.png"
-                        },
-                        "description":
-                            "이번 버스 :  큰 버스\n" +
-                            "6정류장 전,  5분 후 도착.\n" +
-                            "다음 버스 :  작은 버스\n" +
-                            "13정류장 전,  20분 후 도착",
-                        "buttons": [
-                            {
-                                "action": "message",
-                                "label": "다시 조회",
-                                "messageText": Object.stNm
-                            },
-                        ]
-                    }
-                },ㄷ
-            ]
-        }
+    const result = await PromiseParser(data);
+    let resultObject = []
+    let itemList = result.ServiceResult.msgBody.itemList;
+    if(itemList.length > 1){
+        itemList = result.ServiceResult.msgBody.itemList[0];
     }
-    res.status(200).send(responseTeplate);
+    let arrmsg1 = itemList.arrmsg1;
+    let arrmsg2 =itemList.arrmsg2;
+    let veh1 = itemList.vehId1;
+    let veh2 = itemList.vehId2;
+    resultObject.stNm = itemList.stNm;
+    console.log(itemList)
+
+
+    if(arrmsg1 !== "운행종료" ) {
+        switch (arrmsg1) {
+            case "곧 도착":
+                resultObject.description =  "이번 버스 : "+ getBusSize(veh1).label + "\n" + arrmsg1 + "\n";
+                break;
+            case "출발대기":
+                resultObject.description =  "이번 버스 : "+ arrmsg1 + "\n";
+                break
+            default:
+                let split_Minute1 = arrmsg1.split('분')
+                let Station_result1 = split_Minute1[1].split('[')[1]
+                    .slice(0, -1)
+                    .replace("번째", "정류장")
+                resultObject.description = "이번 버스 : " + getBusSize(veh1).label + "\n" + Station_result1 + ", " + split_Minute1[0] + "분 후 도착\n";
+                break;
+
+        }
+
+        if(arrmsg2 !=="출발대기") {
+            let split_Minute2 = arrmsg2.split('분')
+            let Station_result2 = split_Minute2[1].split('[')[1]
+                .slice(0, -1)
+                .replace("번째", "정류장");
+            resultObject.description += "다음 버스 : " + getBusSize(veh2).label + "\n" + Station_result2 + ", " + split_Minute2[0] + "분 후 도착\n";
+        }
+        else{
+            resultObject.description += "다음 버스 : " +arrmsg2;
+
+        }
+        resultObject.Image = "http://13.125.72.93/img/" + getBusImage(getBusSize(veh1).code, getBusSize(veh2).code);
+        res.status(200).send(busResultMessage(resultObject));
+    }else{
+
+    }
+
+    console.log(resultObject)
+
 
 }));
 
---
+++ src/message/BusResultMessage.js
@@ -0,0 +1,31 @@
+export const busResultMessage = (resultObject) => {
+    const responseTeplate = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "basicCard": {
+                        "title": resultObject.stNm,
+                        "thumbnail": {
+                            "fixedRatio":true,
+
+                            "imageUrl": resultObject.Image
+                        },
+                        "description":resultObject.description,
+                        "buttons": [
+                            {
+                                "action": "message",
+                                "label": "다시 조회",
+                                "messageText": resultObject.stNm
+                            },
+                        ]
+                    }
+                },
+            ]
+        }
+    }
+
+    return responseTeplate
+
+}
+
